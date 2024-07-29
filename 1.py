1eef71714dc93a085d29ad820454bad1be2c988b
Bansilal <bansilal@samranit.com>
2024-07-30 01:10:49 +0530

    c2

---
+++ 1.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first value:"))
+b=int(input("Enter the second value:"))
+if a>b:
+    print("first number is greater")
+elif b>a:
+    print("second number is greater")
+else:
+    print("Both are equal")
